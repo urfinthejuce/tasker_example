67e1756e01804adb9ea2f47af49c5a5190af47da
urfinthejuce <42406107+urfinthejuce@users.noreply.github.com>
2018-08-16 20:25:00 +0300

    Еще поигрался с формами

--- src/main/kotlin/ru/tasker/example/RestController.kt
+++ src/main/kotlin/ru/tasker/example/RestController.kt
@@ -1,20 +1,24 @@
 package ru.tasker.example
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Component
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.ModelAndView
-import ru.tasker.example.hello.CounterRepo
 import java.util.*
+import javax.persistence.*
+import javax.validation.Valid
 
 @RestController()
-class RestController(@Autowired val repo: CounterRepo) {
+class RestController(@Autowired val urep: UserRepository) {
   @GetMapping("")
   fun index() = "<html><h1 style=\"text-align: center\">Дратути!</h1></html>"
 
   @GetMapping("/login")
-  fun login(@RequestParam(value = "user") user: String?): ModelAndView? = ModelAndView("login", Collections.singletonMap("login", user))
+  fun login(@RequestParam(value = "user") user: String?): ModelAndView =
+      ModelAndView("login", Collections.singletonMap("login", user))
 
   @PostMapping("/login")
   fun loguser(@ModelAttribute("Credentials") crd: Credentials): String {
@@ -24,7 +28,42 @@ class RestController(@Autowired val repo: CounterRepo) {
   @GetMapping("/logout")
   fun logout() = "Ну и пошел в жопу"
 
+  @GetMapping("/register")
+  fun regForm() = ModelAndView("register")
+
+  @PostMapping("/register")
+  fun register(@Valid @ModelAttribute("user") user: User?, bindingResult: BindingResult?): String {
+    if (!urep.findByLogin(user?.login).isEmpty())
+      return "Уже есть пользователь ${user?.name}"
+    urep.save(user);
+    return "Добро пожаловать, ${user?.name}"
+  }
+
+  @GetMapping("/userlist")
+  fun listUsers(): String {
+    val s = StringBuilder()
+    urep.findAll().forEach({s.append(it).append("<br/>")})
+    return s.toString()
+  }
 }
 
 @Component("Credentials")
-data class Credentials(val login: String="", val password: String="")
+data class Credentials(val login: String = "", val password: String = "")
+
+@Entity
+@Table(name = "user")
+@Component("user")
+data class User(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
+    val login: String? = null,
+    val password: String? = null,
+    val name: String? = null,
+    val birthdate: String? = null) {
+  override fun toString(): String {
+    return "login: $login, name: $name${if (!birthdate.isNullOrBlank()) ", birthdate: $birthdate" else ""}"
+  }
+}
+
+interface UserRepository : CrudRepository<User, Long> {
+  fun findByLogin(login: String?): List<User>
+}
